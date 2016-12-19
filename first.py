764e325ef2c7de7632fbc255263e6bb3191be6c3
zhuhaoyi <zhuhaoyi@163.com>
2016-12-19 10:24:07 +0800

    second

--- first.py
+++ first.py
@@ -1,16 +1,24 @@
-import math
+import json
 
-for x in range(0, 3):
-    def age(num):
-        if num > 18:
-            print('old')
-        elif num < 18:
-            print('young')
-        else:
-            print('i don\'t konw')
+# for x in range(0, 3):
+#     def age(num):
+#         if num > 18:
+#             print('old')
+#         elif num < 18:
+#             print('young')
+#         else:
+#             print('i don\'t konw')
+#
+#     text = input("type your age\nage:")
+#     text = int(text)
+#     age(text)
 
-    text = input("type your age\nage:")
-    text = int(text)
-    age(text)
+# print('完毕')
+#
+# print(json._default_decoder)
 
-print('完毕')
+def register(x,y):
+    print(x)
+    print(y)
+
+register('a','b')
