8398169fc39c3f68af0cb26bf18d03087d333341
zhuhaoyi <zhuhaoyi@163.com>
2016-12-19 12:01:16 +0800

    3

---
+++ 2.py
@@ -0,0 +1,12 @@
+from datetime import datetime
+
+
+
+exit()
+class Teemo(object):
+    def __init__(self,a,b):
+        self.a=111
+        pass
+
+ddd=Teemo(1,1)
+print(ddd.a)
--- first.py
+++ first.py
@@ -1,24 +1,81 @@
-import json
+from tkinter import *
+import tkinter.messagebox as messagebox
 
-# for x in range(0, 3):
-#     def age(num):
-#         if num > 18:
-#             print('old')
-#         elif num < 18:
-#             print('young')
-#         else:
-#             print('i don\'t konw')
-#
-#     text = input("type your age\nage:")
-#     text = int(text)
-#     age(text)
+class Application(Frame):
+    def __init__(self, master=None):
+        Frame.__init__(self, master)
+        self.pack()
+        self.createWidgets()
 
-# print('完毕')
-#
-# print(json._default_decoder)
+    def createWidgets(self):
+        self.nameInput = Entry(self)
+        self.nameInput.pack()
+        self.alertButton = Button(self, text='Hello', command=self.hello)
+        self.alertButton.pack()
 
-def register(x,y):
-    print(x)
-    print(y)
+    def hello(self):
+        name = self.nameInput.get() or 'world'
+        messagebox.showinfo('Message', 'Hello, %s' % name)
 
-register('a','b')
+app = Application()
+# 设置窗口标题:
+app.master.title('Hello World')
+# 主消息循环:
+app.mainloop()
+
+exit()
+import threading, multiprocessing
+
+def loop():
+    x = 0
+    while True:
+        x = x ^ 1
+
+for i in range(multiprocessing.cpu_count()):
+    t = threading.Thread(target=loop)
+    t.start()
+
+
+
+exit()
+import os
+
+print(os.getppid())
+pid = os.fork()
+print(pid)
+
+
+
+exit()
+teemo=[]
+n=1
+while n<=100:
+    teemo.append(n)
+    n=n+3
+print(teemo[3:8])
+
+
+exit()
+for x in range(0, 3):
+    def age(num):
+        if num > 18:
+            print('old')
+        elif num < 18:
+            print('young')
+        else:
+            print('i don\'t konw')
+
+    text = input("type your age\nage:")
+    text = int(text)
+    age(text)
+
+print('完毕')
+
+print(json._default_decoder)
+
+def teemo(*num):
+    for n in num:
+        print(n)
+
+teemo(1,2,3)
+
